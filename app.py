c2ed6f06980c54ec360941d64064b0ebe7c4c7ce
Troy Chroi <troychroi@gmail.com>
2017-10-10 15:47:58 -0700

    update

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
-questions = {
-	"strong": "do you like your drink strong?",
-	"salty": "do you like your drink tangy?",
-	"bitter": "do you like your drink bitter?",
-	"sweet": "do you like your drink sweet?",
-	"fruity": "do you like your drink fruity?",
-}
-
-ingredients = {
-	"strong": ["rum", "whiskey", "gin"],
-	"salty": ["olive", "salted rim", "bacon"],
-	"bitter": ["bitters", "tonic", "lemon"],
-	"sweet": ["sugar", "honey", "cola"],
-	"fruity": ["orange", "cassis", "cherry"],
-}
-
 responses = {}
-drink_ingredients = []
 
-import sys 
 import random
 
+def askQuestions():
+	questions = {
+		"strong": "do you like your drink strong?",
+		"salty": "do you like your drink tangy?",
+		"bitter": "do you like your drink bitter?",
+		"sweet": "do you like your drink sweet?",
+		"fruity": "do you like your drink fruity?",
+	}	
+	for key, value in questions.items():
+		y = True
+		n = False		
+		user_input = input(value+" (y/n?) ")
+		responses.update({key: user_input})
+def constructDrink():
+	ingredients = {
+		"strong": ["rum", "whiskey", "gin"],
+		"salty": ["olive", "salted rim", "bacon"],
+		"bitter": ["bitters", "tonic", "lemon"],
+		"sweet": ["sugar", "honey", "cola"],
+		"fruity": ["orange", "cassis", "cherry"],
+	}	
+	drink_ingredients = []
+	for key, value in responses.items():
+		if(value == True):
+			drink_ingredients.append(random.choice(ingredients[key]))
+	print(drink_ingredients)
+	return drink_ingredients
+def nameDrink():
+	adjectives = [
+		"Potent",
+		"Salty",
+		"Punching",
+		"Cloying",
+		"Tropical"
+	]
+	nouns = [
+		"Bomb",
+		"Dog",
+		"Martini",
+		"Daquiri",
+		"Punch"
+	]
+	return (random.choice(adjectives) + ' ' + random.choice(nouns))
+
 if __name__ == '__main__':
-	
-	def askQuestions():
-		for key, value in questions.items():
-			y = True
-			n = False		
-			user_input = input(value+" (y/n?) ")
-			responses.update({key: user_input})
-		# for key, value in responses.items():
-		# 	print({key: value})
 	askQuestions()
-	def constructDrink():
-		for key, value in responses.items():
-			if(value == True):
-				drink_ingredients.append(random.choice(ingredients[key]))
-		print(drink_ingredients)
 	constructDrink()
+	yesAnother = nameDrink()
+	while True:	
+		y = True
+		n = False
+		another = input("Would you like another "+ yesAnother +"(y/n)? ")
+		if (another == False):
+			break
+		else:
+			print("Here's another " + yesAnother)
